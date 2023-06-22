5aef48042f6ccc884d908c0e63854618eb8396ec
Javier Alvarez <javialsal@gmail.com>
2023-06-22 16:59:30 +0200

    Time is over... final changes before the Living Code

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,4 +1,9 @@
 class MissionsController < ApplicationController
   def index
+    @missions = Mission.all
+
+    @response = { missions: @missions }
+
+    render json: @missions
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
   resources :listings, only: ['index', 'show', 'create', 'update', 'delete']
-  resources :reservations
-  resources :bookings
-  resources :missions
+  resources :missions, only: ['index']
 end
