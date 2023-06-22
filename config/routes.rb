e98b88afffde5df09bc78d2adf28ad5b7fbc00c5
Javier Alvarez <javialsal@gmail.com>
2023-06-22 15:50:20 +0200

    Controllers generated with basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   resources :listings, only: ['index', 'show', 'create', 'update', 'delete']
   resources :reservations
   resources :bookings
+  resources :missions
 end
